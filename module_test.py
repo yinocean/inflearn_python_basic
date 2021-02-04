f9dedf5fbc0a2e514a12deffebd43cb0df58a22e
River <dms930917@naver.com>
2021-02-04 21:57:26 +0900

    2021 02 04 TIL

---
+++ module_test.py
@@ -0,0 +1,19 @@
+# 모듈 사용 실습
+
+import sys
+print(sys.path)
+
+print(type(sys.path))
+
+#sys.path.append('C:/math')
+
+#print(sys.path)
+
+#import test_module
+
+# 모듈 사용
+#print(test_module.power(10, 3))
+
+import chapter06_02
+
+print(chapter06_02.add(10, 199))
