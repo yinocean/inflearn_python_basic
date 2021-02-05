088a2d879b6538812ecd424bd91f12e9b51b2aad
River <dms930917@naver.com>
2021-02-05 23:46:01 +0900

    2021 02 05 TIL

---
+++ chapter06_03.py
@@ -0,0 +1,49 @@
+# Chapter06-3
+# 파이썬 패키지
+# 패키지 작성 및 사용법
+# 파이썬은 패키지로 분할된 개별적인 모듈로 구성
+# __init__.py : Python 3.3 부터는 없어도 패키지로 인식 -> 단, 하위 호환을 위해 작성추천
+# 상대경로 : ..(부모 디렉토리), .(현재 디렉토리) -> 모듈 내부에서만 사용
+
+# 예제1
+import sub.sub1.module1
+import sub.sub2.module2
+
+# 사용
+sub.sub1.module1.mod1_test1()
+sub.sub1.module1.mod1_test2()
+
+sub.sub2.module2.mod2_test1()
+sub.sub2.module2.mod2_test2()
+
+print()
+print()
+print()
+
+# 예제2
+from sub.sub1 import module1
+from sub.sub2 import module2 as m2 # alias
+
+module1.mod1_test1()
+module1.mod1_test2()
+
+print()
+print()
+print()
+
+# 예제3
+from sub.sub1 import *
+from sub.sub2 import *
+
+module1.mod1_test1()
+module1.mod1_test2()
+
+module2.mod2_test1()
+module2.mod2_test2()
+
+# 예제 4
+print(module1.add(11, 2))
+print(module1.subtract(9, 17))
+print(module1.divide(5, 2))
+print(module1.multiply(50, 2.4))
+print(module1.power(10, 5))
