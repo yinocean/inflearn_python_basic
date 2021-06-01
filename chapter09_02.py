fe2d61ca7c4ea1cbfb6aa6e4df4cb4efa548df6e
river0123 <dms930917@naver.com>
2021-06-01 15:33:01 +0900

    2021 06 01 TIL

--- chapter09_02.py
+++ chapter09_02.py
@@ -1,3 +1,4 @@
+# river0123
 # Chapter09-2
 # CSV 파일 읽기 및 쓰기
 
@@ -5,10 +6,10 @@
 
 import csv
 
-# 에제1
-with open('./resource/test1.csv', 'r') as f:
+# 예제1
+with open('./resource/write1.csv', 'r') as f:
     reader = csv.reader(f)
-
+    # a = next(reader) # Header Skip
     # 객체 확인
     print(reader)
 
@@ -19,4 +20,57 @@ with open('./resource/test1.csv', 'r') as f:
     print()
 
     for c in reader:
+        # print(c)
+        # 타입 확인(리스트)
+        # print(type(c))
+        # list to str
+        print(' : '.join(c))
+
+# 예제2 구분자 사용
+with open('./resource/test2.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+
+    for c in reader:
         print(c)
+
+# 예제3 딕셔너리리더 사용
+with open('./resource/test1.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    # 확인
+    print(reader)
+    print(type(reader))
+    print(dir(reader))
+    print()
+
+    for c in reader:
+        for k, v in c.items():
+            print(k, v)
+        print('-------')
+
+# 예제4
+w = [[1,2,3], [4,5,6], [7,8,9], [10,11,12], [13,14,15],[16,17,18], [19,20,21]]
+
+with open('./resource/write1.csv', 'w', encoding='UTF-8') as f:
+    print(dir(csv))
+    wt = csv.writer(f)
+
+    # dir 확인
+    # print(dir(wt))
+    # 타입 확인
+    # print(type(wt))
+
+    for v in w:
+        wt.writerow(v)
+
+# 예제5
+with open('./resource/write2.csv', 'w', encoding='UTF-8') as f:
+    # 필드명
+    fields = ['One', 'Two', 'Three']
+
+    # Dict Writer
+    wt = csv.DictWriter(f, fieldnames=fields)
+    # Header writer
+    wt.writeheader()
+
+    for v in w:
+        wt.writerow({'One':v[0], 'Two':v[1], 'Three':v[2]})
